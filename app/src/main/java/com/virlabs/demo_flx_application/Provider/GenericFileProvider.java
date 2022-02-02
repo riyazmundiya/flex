3142452f0b8c5c8d288768b778cfe972ad6b61a3
riyaz mundiya <rrmundiya@gmail.com>
2022-02-02 16:56:08 +0530

    flex

---
+++ app/src/main/java/com/virlabs/demo_flx_application/Provider/GenericFileProvider.java
@@ -0,0 +1,8 @@
+package com.virlabs.demo_flx_application.Provider;
+
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+
+}
---
+++ app/src/main/java/com/virlabs/demo_flx_application/Utils/Log.java
@@ -0,0 +1,10 @@
+package com.virlabs.demo_flx_application.Utils;
+
+/**
+ * Created by Tamim on 21/3/17.
+ */
+public class Log {
+    public static void log(String msg) {
+        android.util.Log.d("Nzm",""+msg);
+    }
+}
